814028ab54516513fbffc8e5f45beedab387008b
stbmte <ateruhin388@gmail.com>
2023-03-24 21:55:34 +0300

    Initial commit

---
+++ src/test/java/com/example/homebookkeeping/HomeBookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homebookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
